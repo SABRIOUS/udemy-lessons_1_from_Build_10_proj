9308fd00fa5b091354810ee51f7270a3d8a4c838
SABRIOUS <ahmedsabrious@gmail.com>
2019-06-18 22:44:46 +0200

    new Version of dictionary

---
+++ app1.py
@@ -0,0 +1,36 @@
+import json
+import difflib
+from difflib import SequenceMatcher
+from difflib import get_close_matches
+data = json.load(open("data.json"))
+
+
+def translate(w):
+    w = w.lower()
+    if w in data:
+        return data[w]
+    elif w.title() in data:
+        return data[w.title()]
+    elif w.upper() in data: #in case user enters words like USA or NATO
+        return data[w.upper()]
+    elif len(get_close_matches(w,data.keys())) >0:
+        Yes_No = input("Did you mean >>> %s <<< instead ?:Enter Y if Yes , N if No "%  get_close_matches(w,data.keys())[0])
+        if Yes_No == "Y":
+            return data[get_close_matches(w,data.keys())[0]]
+        elif Yes_No == "N":
+            return "word does not exist "
+        else:
+            return "Nothing Here"
+
+    elif w.lower() in data:
+        return "Please enter a lower name"
+    return "The word doesn't exist"
+
+word = input("Enter word: ")
+output = translate(word)
+
+if type(output) == list:
+    for i in output:
+        print(i)
+else:
+    print(output)
